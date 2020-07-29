732fb790944fa236cc08bbb6c52bbca13166670e
brody sullivan <brodysullivan0@gmail.com>
2020-07-29 02:04:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
     SELECT * FROM students WHERE name = ?
     LIMIT 1
     SQL
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute(
     end.first
   end
   
